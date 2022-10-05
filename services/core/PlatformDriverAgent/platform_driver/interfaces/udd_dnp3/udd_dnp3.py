f43e2e74c00253c5cff722500a76a7c92cd49ffe
Kefei Mo <kefeimo@gmail.com>
2022-10-05 14:42:02 -0400

    modified dnp3 driver behavior when there is no data

--- services/core/PlatformDriverAgent/platform_driver/interfaces/udd_dnp3/udd_dnp3.py
+++ services/core/PlatformDriverAgent/platform_driver/interfaces/udd_dnp3/udd_dnp3.py
@@ -74,11 +74,15 @@ class UserDevelopRegisterDnp3(WrapperRegister):
             val = self._get_outstation_pt(self.master_application, group, variation, index)
             # val = str(val)
 
-            return val
+            if val is not None:
+                return val
+            else:
+                raise ValueError(f"Returned invalid dnp3 data point {val}")
         except Exception as e:
             # print(f"!!!!!!!!!!!!!!!!!!!!{e}")
             _log.error(e)
             _log.warning("udd_dnp3 driver (master) couldn't collect data from the outstation.")
+            raise Exception(e)
 
     @staticmethod
     def _get_outstation_pt(master_application, group, variation, index) -> RegisterValue:
